65fa4a9341b8940f2a3213d7d5e85ebd1b45615c
Claudia Fox <claudiafox95@icloud.com>
2018-11-14 21:01:25 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,9 @@ require "pry"
 def game_hash
   {
     home: {
-      :team_name: "Brooklyn Nets",
-      :colors: ["Black", "White"],
-      :players: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
         "Alan Anderson" => {number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
         "Reggie Evans" => {number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
         "Brook Lopez" => {number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15},
@@ -14,9 +14,9 @@ def game_hash
       }
     },
     away: {
-      :team_name: "Charlotte Hornets",
-      :colors: ["Turquoise", "Purple"],
-      :players: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
         "Jeff Adrien" => {number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
         "Bismak Biyombo" => {number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10},
         "DeSagna Diop" => {number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5},
